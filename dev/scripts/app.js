45e3604abe641829ccbfa91ed93223617b5312b5
Anson Li <incendie323@gmail.com>
2017-09-06 16:18:02 -0400

    update with react router

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,1 +1,12 @@
 console.log("ewoks");
+class Home extends React.Component {
+	render(){
+		return(
+			<div>
+				<h1>Test</h1>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
